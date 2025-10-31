85f62638666306b5085c8bfc5f6c598aa160cf5d
Vitor Silva De Souza <vitor2071@gmail.com>
2025-10-31 11:43:21 -0300

    feat: tarefa 4 nivel 1. Trabalhando com set para eliminar elementos duplicados de uma lista

---
+++ nivel1/04.dart
@@ -0,0 +1,7 @@
+//Cadastro Único (Set): Crie um Set<int> chamado idsUnicos e adicione os seguintes IDs: 101, 202, 101, 303, 404, 202. Imprima o Set para confirmar que os IDs duplicados foram ignorados.
+
+main() {
+  final Set<int> ids = {101, 202, 101, 303, 404, 202};
+
+  print(ids); 
+}
