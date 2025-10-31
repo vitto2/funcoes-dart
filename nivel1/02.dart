6b01e5ac159ed85a9b82f4bb4d4e0564f9dc2111
Vitor Silva De Souza <vitor2071@gmail.com>
2025-10-31 11:36:53 -0300

    feat: implementa tarefa 02 nivel 1. Usando foreach para imprimir itens de uma lista

---
+++ nivel1/02.dart
@@ -0,0 +1,8 @@
+//Lista de Compras: Crie uma List<String> chamada listaDeCompras com 5 itens (pode haver repetição). Use o método forEach para imprimir cada item da lista com o prefixo
+
+main() {
+  final List<String> listaDeCompras = ['Arroz', 'Feijão', 'Sabão', 'Carne', 'Alho'];
+
+
+  listaDeCompras.forEach( (item) => print(item));
+}
